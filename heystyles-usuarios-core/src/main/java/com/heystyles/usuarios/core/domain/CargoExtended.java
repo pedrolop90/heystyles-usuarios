65d078c820df3f8b16aecdca368841d37aa09629
pedrolop900 <pedro.lopez@orionsoftware.com.co>
2019-10-30 02:45:16 -0500

    cambios fecha "2019-10-30" 02:45 am ....

--- heystyles-usuarios-core/src/main/java/com/heystyles/usuarios/core/domain/CargoExtended.java
+++ heystyles-usuarios-core/src/main/java/com/heystyles/usuarios/core/domain/CargoExtended.java
@@ -1,20 +1,20 @@
 package com.heystyles.usuarios.core.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import domain.PermisoAuth0;
+import domain.PermisoAuth0Extended;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CargoExtended extends Cargo {
 
-    private List<PermisoAuth0> permisos;
+    private List<PermisoAuth0Extended> permisos;
 
-    public List<PermisoAuth0> getPermisos() {
+    public List<PermisoAuth0Extended> getPermisos() {
         return permisos;
     }
 
-    public void setPermisos(List<PermisoAuth0> permisos) {
+    public void setPermisos(List<PermisoAuth0Extended> permisos) {
         this.permisos = permisos;
     }
 }
