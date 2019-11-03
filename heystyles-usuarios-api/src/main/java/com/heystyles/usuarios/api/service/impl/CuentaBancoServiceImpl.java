293284c8a116f4845920d265bfb218f1d869edc0
pedrolop900 <pedro.lopez@orionsoftware.com.co>
2019-11-03 18:02:14 -0500

    cambios fecha "2019-11-03" 05:00 pm ....

--- heystyles-usuarios-api/src/main/java/com/heystyles/usuarios/api/service/impl/CuentaBancoServiceImpl.java
+++ heystyles-usuarios-api/src/main/java/com/heystyles/usuarios/api/service/impl/CuentaBancoServiceImpl.java
@@ -8,8 +8,6 @@ import com.heystyles.usuarios.core.domain.CuentaBanco;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
