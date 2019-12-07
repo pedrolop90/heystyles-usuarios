a55a366b3143b3465e2fd23e37d2066cc517c37a
pedrolop900 <pedro.lopez@orionsoftware.com.co>
2019-12-07 16:42:09 -0500

    ...

---
+++ usuarios-cliente/src/main/java/com/heystyles/usuarios/cliente/CargoClient.java
@@ -0,0 +1,9 @@
+package com.heystyles.usuarios.cliente;
+
+import com.heystyles.usuarios.core.domain.Cargo;
+
+public interface CargoClient {
+
+    Cargo getCargo(Long cargoId);
+
+}
---
+++ usuarios-cliente/src/main/java/com/heystyles/usuarios/cliente/impl/CargoClienteImpl.java
@@ -0,0 +1,34 @@
+package com.heystyles.usuarios.cliente.impl;
+
+import com.heystyles.usuarios.cliente.CargoClient;
+import com.heystyles.usuarios.core.domain.Cargo;
+import com.heystyles.usuarios.core.dto.CargoResponse;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class CargoClienteImpl implements CargoClient {
+
+    private final RestTemplate client;
+
+    private final String urlBase;
+
+    private interface SegmentPaths {
+        String CARGO = "cargos";
+    }
+
+    public CargoClienteImpl(String urlBase, RestTemplate client) {
+        this.client = client;
+        this.urlBase = urlBase;
+    }
+
+    @Override
+    public Cargo getCargo(Long cargoId) {
+        UriComponentsBuilder urlBuilder = getUriProveedor()
+                .pathSegment(String.valueOf(cargoId));
+        return client.getForEntity(urlBuilder.toUriString(), CargoResponse.class).getBody().getData();
+    }
+
+    private UriComponentsBuilder getUriProveedor() {
+        return UriComponentsBuilder.fromHttpUrl(urlBase).pathSegment(SegmentPaths.CARGO);
+    }
+}
